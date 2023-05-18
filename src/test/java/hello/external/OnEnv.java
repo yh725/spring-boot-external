858837178123e6289b9f8b4c60e0495af0872a2e
yh725 <yh725@kakao.com>
2023-05-18 18:59:30 +0900

    외부설정과 프로필1 - 프로젝트 설정 - 외부 설정이란? - 외부 설정(OS 환경 변수) - 외부 설정(자바 시스템 속성) - 외부 설정(커맨드 라인 인수) - 외부 설정(커맨드 라인 옵션 인수) - 외부 설정(커맨드 라인 옵션 인수와 스프링 부트)

---
+++ src/test/java/hello/external/OnEnv.java
@@ -0,0 +1,20 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class OnEnv {
+
+	public static void main(String[] args) {
+		Map<String, String> envMap = System.getenv();
+		for (String key : envMap.keySet()) {
+			log.info("env {}={}", key, System.getenv(key));
+		}
+
+		// DBURL=dev.db.com 개발서버
+		// DBURL=prod.db.com 운영서버
+		System.getenv("DBURL");
+	}
+}
